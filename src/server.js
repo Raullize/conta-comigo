9bb903a2f65eee4665a6f5c49b7ebce27b9d25ca
Thiago Caputi <lcaputi2@gmail.com>
2025-05-28 11:03:22 -0300

    build: adciona server.js e database.js

--- src/server.js
+++ src/server.js
@@ -1,1 +1,13 @@
- 
+// 1. Importação com require()
+const app = require('./app.js');
+const database = require('./database/database.js');
+
+const port = 3033;
+
+// Sincroniza o banco de dados
+database.sync({ alter: true });
+
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server running at http://localhost:${port}/`);
+});
