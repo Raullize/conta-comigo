8db625642aa7f42c4a2cec3bbf0a97e5dd34cb64
Vitor Km <vhkm12@gmail.com>
2025-05-28 10:11:18 -0300

    Corrigindo users

---
+++ src/database/migrations/20250528130346-create-categories.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('accounts', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      institution: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('accounts');
+  },
+};
