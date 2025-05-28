43041f4bb3599608c3844cea39b295b940828f4c
Vitor Km <vhkm12@gmail.com>
2025-05-28 09:52:13 -0300

    Adicionando migrations goals

---
+++ src/database/migrations/20250528124027-create-goals.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('goals', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      value: {
+        type: Sequelize.NUMBER,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('goals');
+  },
+};
