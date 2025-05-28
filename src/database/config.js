59e12f5b6477ae4e2e144fa28b152bd70bd2acd0
Vitor Km <vhkm12@gmail.com>
2025-05-28 17:24:35 -0300

    Build: Implementado o docker + fix: Correção no banco

---
+++ src/database/config.js
@@ -0,0 +1,14 @@
+// src/database/config.js
+const dotenv = require('dotenv');
+dotenv.config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+  },
+};
