9f5d3288f67db64808e80fbb6bfbbeea63b6cbd4
Lucas Guidotti <lucasguidottipro@gmail.com>
2025-05-28 12:53:31 -0300

    Feat: Adiciona classe e definições ao model User.js

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -4,24 +4,45 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+
     static associate(models) {
-      // define association here
+      User.hasMany(models.Goal, {
+        foreignKey: 'user_id',
+        as: 'goals'
+      });
     }
   }
+
   User.init({
-    cpf: DataTypes.STRING,
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    birth_date: DataTypes.STRING,
-    password: DataTypes.STRING
+    cpf: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+      validate: {
+        isEmail: true
+      }
+    },
+    birth_date: {
+      type: DataTypes.DATEONLY
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   }, {
     sequelize,
     modelName: 'User',
+    tableName: 'users',
+    
   });
   return User;
 };
