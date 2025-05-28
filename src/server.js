59c90e5bf08b92fe6d2b64ccce11d04edb20a171
Thiago Caputi <lcaputi2@gmail.com>
2025-05-28 12:30:08 -0300

    fix: remocao comentarios server.js

--- src/server.js
+++ src/server.js
@@ -1,10 +1,8 @@
-// 1. Importação com require()
 const app = require('./app.js');
 const database = require('./database/database.js');
 
 const port = 3033;
 
-// Sincroniza o banco de dados
 database.sync({ alter: true });
 
 app.listen(port, () => {
