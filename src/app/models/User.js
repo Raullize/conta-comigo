f0dde77de696f2f588ee0e61a57b1d64ccc70838
Lucas Guidotti <lucasguidottipro@gmail.com>
2025-05-29 11:17:03 -0300

    build: ajusta código do Users.js

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
       }),
 
       User.hasMany(models.Account, {
-        foreignKey: user_id,
+        foreignKey: 'user_id',
         as: 'accounts',
       }),
     }
