823aa9c6a55b8b1df81ea20c1295c48c1ff6226f
Lucas Guidotti <lucasguidottipro@gmail.com>
2025-05-30 15:43:30 -0300

    feat: adiciona o método createAccount no userController.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
 
-const userController = require('../app/controllers/UserController');
+const userController = require('../app/controllers/userController');
 const sessionController = require('../app/controllers/sessionController');
 const categoryController = require('../app/controllers/categoryController');
+const accountController = require('../app/controllers/accountController');
 
 const routes = new Router();
 
@@ -10,5 +11,7 @@ routes.post('/users', userController.store);
 routes.post('/sessions', sessionController.store);
 
 routes.post('/category', categoryController.createCategory);
+
+routes.post('/account', accountController.createAccount);
 
 module.exports = routes;
