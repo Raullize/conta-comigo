b63c4db772bc8c4d28125dc01847bcc82f9a285c
patriciapqa <patriciaquirozadolpho@gmail.com>
2025-05-30 11:09:22 -0300

    build: middleware com token, chore: manutenção do userController

--- src/app/controllers/userController.js
+++ src/app/controllers/userController.js
@@ -15,9 +15,7 @@ class userController {
       email,
     });
 
-   
-
-    
+    as;
   }
 }
 module.exports = new userController();
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,13 +1,21 @@
 const { Router } = require('express');
 
-const userController = require('../app/controllers/UserController');
+const authMiddleware = require('../app/middlewares/auth');
+
+const userController = require('../app/controllers/userController');
 const sessionController = require('../app/controllers/sessionController');
 const categoryController = require('../app/controllers/categoryController');
 
 const routes = new Router();
 
 routes.post('/users', userController.store);
+
 routes.post('/sessions', sessionController.store);
+
+//tudo abaixo dessa rota vai trancar, tem que por token
+routes.use(authMiddleware);
+
+routes.put('/users', userController.update);
 
 routes.post('/category', categoryController.createCategory);
 
