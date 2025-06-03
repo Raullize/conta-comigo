d140887680718a5fbea9eca90299abd0da52fc23
Lucas Guidotti <lucasguidottipro@gmail.com>
2025-06-03 10:12:11 -0300

    feat: adiciona módulos getAllAccounts, deleteAccount e updateAccount no controller accountController.js

--- src/app/controllers/accountController.js
+++ src/app/controllers/accountController.js
@@ -1,5 +1,6 @@
-const Account = require('../models/Account');
-const User = require('../models/User');
+const { where } = require('sequelize');
+const Account = require('../models/Account');   
+const { alternatives } = require('joi');
 
 module.exports = {
     async createAccount(req, res){
@@ -9,11 +10,11 @@ module.exports = {
             } = req.body;
 
             if(!userId){
-                return res.status(404).json({error: "The user doesn't exists"});
+                return res.status(404).json({ error: "The user doesn't exists" });
             }
 
             if(!institution){
-                return res.status(400).json({error: 'Missing required account fields.'});
+                return res.status(400).json({ error: 'Missing required account fields.' });
             }
 
         try {
@@ -25,7 +26,7 @@ module.exports = {
             });
 
             if(accountExists){
-                return res.status(400).json({ error: 'This account already exists for this user.'});
+                return res.status(400).json({ error: 'This account already exists for this user.' });
             }
 
             const newAccount = await Account.create({
@@ -36,31 +37,67 @@ module.exports = {
             return res.status(201).json(newAccount);
 
         } catch (error) {
-            return res.status(500).json({erro: 'Erro ao criar conta.', detalhe: error.message });
+            return res.status(500).json({erro: 'Failed to creater account.', detalhe: error.message });
         }
     },
 
     async updateAccount(req, res){
+        const { id } = req.params;
+        const userId = req.userId;
+
         try {
-            
+            const account = await Account.findOne({
+                where: { id, userId },
+            });
+
+            if(!account){
+                return res.status(404).json({ error: 'Account not found or does not belong to this user.' });
+            }
+
+            const updateAccount = await account.update(req.body);
+
+            return res.json(updateAccount);
+
         } catch (error) {
-            
+            return res.status(500).json({ eroor: 'Failed to update account.' });
         }
     },
 
     async getAllAccounts(req, res){
         try {
-            
+             const account = await Account.findOne({
+                where: { id, userId },
+            });
+
+            if(!account){
+                return res.status(404).json({ error: 'Account not found or does not belong to this user.' });
+            }
+
+            return res.json(account);
         } catch (error) {
-            
+            return res.status(500).json({ eroor: 'Failed to update account.' });
         }
     },
 
     async deleteAccount(req, res){
+        const { id } = req.params;
+        const userId = req.userId;
+
         try {
-            
+            const account = await Account.findOne({
+                where: { id, userId },
+            });
+
+            if(!account){
+                return res.status(404).json({ error: 'Account not found or does not belong to this user.' });
+            }
+
+            await account.destroy();
+
+            return res.status(204).send();
+
         } catch (error) {
-            
+            return res.status(500).json({ error: 'Failed to delete account.' });
         }
     }
 }
